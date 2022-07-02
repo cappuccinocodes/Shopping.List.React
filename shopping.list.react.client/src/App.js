f13918f0805ff8acf06314722de6b46d7922b1bf
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-07-03 06:46:19 +1000

    Created empty shopping list component

---
+++ shopping.list.react.client/src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import ShoppingList from './Components/ShoppingList/ShoppingList';
+
+function App () {
+  return (
+    <Fragment>
+      <main>
+        <ShoppingList></ShoppingList>
+      </main>
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ shopping.list.react.client/src/Components/ShoppingList/ShoppingList.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const ShoppingList = () => {
+  return (
+    <Fragment>
+      <h1>Shopping List!</h1>
+    </Fragment>
+  );
+};
+
+export default ShoppingList;
